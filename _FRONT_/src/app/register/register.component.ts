93ff14c627319f93a1a027597d7c9d23ee0512b5
Inez <segers.inez@gmail.com>
2019-05-17 15:10:21 +0200

    register

--- _FRONT_/src/app/register/register.component.ts
+++ _FRONT_/src/app/register/register.component.ts
@@ -14,9 +14,15 @@ export class RegisterComponent {
 
   // ----- POST -----
   onSubmit(person){
+    person.admin_idadminRights = '0';
+    person.status_idstatus = '1';
+
     this.http.post(this.POST_SERVER_URL, person, {responseType: 'text'})
     .subscribe((status)=> {
        console.log(JSON.stringify(status));
+
+       let myContainerForm = <HTMLFormElement> document.getElementById('form');
+       myContainerForm.reset();
     });
   }
 }
