b0f4f6baf31024d200589ef04703380a09ee2daf
Inez <segers.inez@gmail.com>
2019-05-19 14:49:49 +0200

    added routes

--- _FRONT_/src/app/app-routing.module.ts
+++ _FRONT_/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},    // app navigates automatically to home
+  { path: 'home', component: HomeComponent },
+  { path: 'registreren', component: RegisterComponent },
+  { path: 'gebruikers', component: UsersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- _FRONT_/src/app/app.module.ts
+++ _FRONT_/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    RegisterComponent
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
